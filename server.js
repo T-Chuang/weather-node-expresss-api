cf9e4c730a4ea5fb1fb04ae1083b2558e583ea1f
Theodore Chuang <chuangtheodore@gmail.com>
2017-11-03 06:50:08 +1100

    Fix bug, App calls API correctly.

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
-const express = require('express')
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 const app = express()
 
+const apiKey = '62171b3fb1e84eae3c503c5ef08f7276';
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index', {weather: null, error: null});
+})
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 })
 
 app.listen(3000, function () {
